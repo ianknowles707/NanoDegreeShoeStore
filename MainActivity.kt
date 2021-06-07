fc2108779ddbfc357fdee07eb58cee25b004c081
ianknowles707 <72895539+ianknowles707@users.noreply.github.com>
2021-06-07 12:32:38 -0400

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+        val navController = this.findNavController(R.id.navHostFragment)
+
+
+    }
+}
